f49bd2a2ee044cf6fef9b56a9bd54b66bbe64b41
JsomeDev <wnthal1211@naver.com>
2022-11-17 18:53:34 +0900

    feat: callback function

--- NodeJS/221117/추가실습-callback.js
+++ NodeJS/221117/추가실습-callback.js
@@ -1,17 +1,43 @@
+// function call(name) {
+//   console.log("사용자는 " + name);
+//   return "call";
+// }
+// function back() {
+//   return "back";
+// }
+// function hell() {
+//   return "hell";
+// }
+
+// let a = call("kim");
+// console.log("시작은 " + a);
+// let b = back();
+// console.log("두번째는 " + b);
+// let c = hell();
+// console.log("세번째는 " + c);
+
+////
+
 function call(name) {
-  console.log("사용자는 " + name);
-  return "call";
-}
-function back() {
-  return "back";
-}
-function hell() {
-  return "hell";
+  setTimeout(function () {
+    console.log("사용자는 " + name);
+  }, 2000);
 }
 
-let a = call("kim");
-console.log("시작은 " + a);
-let b = back();
-console.log("두번째는 " + b);
-let c = hell();
-console.log("세번째는 " + c);
+function back(b) {
+  setTimeout(function () {
+    console.log("두번째는 back");
+  }, 1000);
+}
+
+function hell(c) {
+  setTimeout(function () {
+    console.log("세번째는 " + c);
+  }, 500);
+}
+
+call("kim", function () {
+  back(function () {
+    hell("hell");
+  });
+});
