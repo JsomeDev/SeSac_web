bded2d1ca2cac9320b496dee08530abb9daf8727
Somi Joo <wnthal1211@naver.com>
2022-11-23 18:34:58 +0900

    feat: update

--- NodeJS/221122_express/실습30.js
+++ NodeJS/221122_express/실습30.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const app = express();
-const port = 8080 ;
+const port = 8080;
 
-app.set("view engine","ejs");
-app.use('/views', express.static(__dirname + '/views'));
+app.set("view engine", "ejs");
+app.use("/views", express.static(__dirname + "/views"));
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/info", (req, res) => {
+  res.render("실습30");
+  console.log(req.query);
+});
 
-app.get("/info" , (req,res) => {
-    res.render("실습30");
-    console.log(req.query);
+app.get("/form", (req, res) => {
+  res.render("실습31");
+});
+
+app.post("/postForm", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(port, () => {
-    console.log("server is open: ", port)
-})
+  console.log("server is open: ", port);
+});
