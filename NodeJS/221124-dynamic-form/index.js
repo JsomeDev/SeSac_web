4c346fa69c3ec2b9c76dd3283e65d9eafe08c7dd
JsomeDev <wnthal1211@naver.com>
2022-11-24 14:33:42 +0900

    feat: dynamic form

---
+++ NodeJS/221124-dynamic-form/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const port = 8000;
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json()); 
+
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/form", (req,res) => {
+  console.log(req.query);
+  res.send("이름은 : " +req.query.name)
+});
+
+app.listen(port, () => {
+  console.log("Server Port : ", port);
+});
