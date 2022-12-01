dd61a147207238ba1a1207f9e5821beba4a92238
JsomeDev <wnthal1211@naver.com>
2022-12-01 18:23:14 +0900

    feat: 221201-mvc-mysql

---
+++ NodeJS/221201-mvc-mysql/model/Visitor.js
@@ -0,0 +1,32 @@
+const mysql = require("mysql");
+
+const cnn = mysql.createConnection({
+  host: "localhost",
+  user: "user",
+  password: "mama5110",
+  database: "somdata",
+});
+
+exports.get_visitor = (cb) => {
+  var sql = "SELECT * FROM visitor";
+
+  cnn.query(sql, (err, rows) => {
+    if (err) throw err;
+
+    console.log("visitors: ", rows);
+    cb(rows);
+  });
+};
+
+exports.register_visitor = (info, cb) => {
+  //info = req.body; {name : , comment: }
+
+  var sql = `insert into visitor(name, comment) values ('${info.name}','${info.comment}')`;
+
+  cnn.query(sql, (err, result) => {
+    if (err) throw err;
+
+    console.log("insert result: ", result);
+    cb(result.insertId);
+  });
+};
